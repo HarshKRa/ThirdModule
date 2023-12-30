087f9c6f33406753244f62ba00271b1579e2e4ee
Harsh Raj Kumar <harsh726181@gmail.com>
2023-12-30 11:50:01 +0530

    All setpup config and code is working fine

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,30 @@
+import {v2 as cloudinary} from 'cloudinary';
+import fs from "fs"
+
+          
+cloudinary.config({ 
+  cloud_name:process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret:process.env.CLOUDINARY_API_SECRET 
+});
+
+
+const uploadOnCloudinary = async (loaclFilePath)=>{
+    try {
+        if(!loaclFilePath) return "Could not find loaclFilePath"
+        // Uload the file path on cloudinary
+        const response = await cloudinary.uploader.upload(loaclFilePath,{
+            resource_type: "auto"
+        })
+
+        // file has been uloaded succesfully
+        console.log("file is uploaded on cloudinary", response, response.url);
+        return response.url;
+    } catch (error) {
+        fs.unlinkSync(loaclFilePath)   // remove the locally saved tem file as the upload opeartion got failed
+        return null
+    }
+}
+
+
+export {uploadOnCloudinary}
